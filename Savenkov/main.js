08b6d43e167c1390be4d768fa74c1c27162394e3
Xaleaq <denissavenkov759@gmail.com>
2024-10-08 17:01:02 +0600

    JavaScript
    work to Js

---
+++ Savenkov/main.js
@@ -0,0 +1,33 @@
+// let str = `Hello`;
+
+// alert( str[0] );
+// alert( str.at(0) );
+
+// alert( str[str.length - 1] );
+// alert( str.at(-1) );
+
+// function showMood(){
+//     const mood = prompt("Введите свое настроение:");
+//     console.log(mood.toUpperCase());
+//     console.log(mood.toLowerCase());
+// }
+
+// showMood();
+
+
+function changeTextAndColor(){
+    const setup = document.getElementById('setup');
+    setup.textContent = 'Viscal Barca!';
+    setup.style.color = 'blue'
+}
+
+// let str = "Строка";
+
+// let newStr = str.slice(0, 3) + str.slice(4);
+// console.log("Строка без 4-го символа:", newStr);
+
+// let firstChar = newStr[0];
+// let lastChar = newStr[newStr.length - 1];
+
+// console.log("Первый символ:", firstChar);
+// console.log("Последний символ:", lastChar);
